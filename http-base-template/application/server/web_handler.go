d3b1284048c3f775373d52bd5cfdce8ebf275ef1
francojposa <franco@francoposa.io>
2020-12-02 10:53:21 -0600

    Working HTML base example

---
+++ http-base-template/application/server/web_handler.go
@@ -0,0 +1,37 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type WebHandler struct {
+	templateRenderer     *TemplateRenderer
+	loginTemplateName    string
+	registerTemplateName string
+}
+
+func NewWebHandler(
+	templateRenderer *TemplateRenderer,
+) *WebHandler {
+	return &WebHandler{
+		templateRenderer: templateRenderer,
+	}
+}
+
+func (h *WebHandler) GetLogin(w http.ResponseWriter, r *http.Request) {
+	err := h.templateRenderer.RenderTemplate(w, "sign-in", "")
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+}
+
+func (h *WebHandler) GetRegister(w http.ResponseWriter, r *http.Request) {
+	err := h.templateRenderer.RenderTemplate(w, "sign-up", "")
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
